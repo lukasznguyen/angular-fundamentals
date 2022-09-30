58df21e9f775519f6842f724b5ba6d9ecf053be7
Tuan Viet Nguyen <lukasznguyen1999@gmail.com>
2022-09-30 22:52:37 +0200

    Interpolation and expressions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string;
+  isHappy: boolean = false;
+  numberOne: number = 1;
+  numberTwo: number = 2;
 
   constructor() {
     this.title = 'Ultimate Angular';
