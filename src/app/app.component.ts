6dde34e69b21f9b12da866b3a2c706af960a0b20
Tuan Viet Nguyen <lukasznguyen1999@gmail.com>
2022-09-30 22:32:22 +0200

    First component with @Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-fundamentals';
+  title: string;
+
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
