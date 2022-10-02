87aebc4373909875130cfaa2a37d48e327b25fcd
Tuan Viet Nguyen <lukasznguyen1999@gmail.com>
2022-10-02 22:47:34 +0200

    Template #ref variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name: string = 'Lucas';
 
-  handleClick() {
-    this.name = 'Skywalker';
-  }
-
-  handleChange(value: string) {
-    this.name = value;
+  handleClick(value: string) {
+    console.log(value);
   }
 }
