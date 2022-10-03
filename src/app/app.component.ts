e044e37e8dde3231e8b0b8b34585e01e719fbc07
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-03 09:09:06 +0200

    ngFor and iteration collections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name: string = '';
-
-  handleChange(value: any) {
-    this.name = value.target.value;
-  }
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true,
+    },
+    {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false,
+    },
+    {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true,
+    },
+    {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true,
+    },
+    {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false,
+    },
+  ];
 }
