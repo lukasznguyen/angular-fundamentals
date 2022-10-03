c64f8990ecfe25a82f6f579458958a74f108bf82
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-03 11:43:26 +0200

    Pipes for data transformation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ interface Passenger {
   id: number;
   fullname: string;
   checkedIn: boolean;
+  checkInDate: number | null;
 }
 
 @Component({
@@ -17,26 +18,31 @@ export class AppComponent {
       id: 1,
       fullname: 'Stephen',
       checkedIn: true,
+      checkInDate: 1490742000000,
     },
     {
       id: 2,
       fullname: 'Rose',
       checkedIn: false,
+      checkInDate: null,
     },
     {
       id: 3,
       fullname: 'James',
       checkedIn: true,
+      checkInDate: 1491606000000,
     },
     {
       id: 4,
       fullname: 'Louise',
       checkedIn: true,
+      checkInDate: 1488412800000,
     },
     {
       id: 5,
       fullname: 'Tina',
       checkedIn: false,
+      checkInDate: null,
     },
   ];
 }
