7b74fd62898820f7e39596e1012a14b8196fa069
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-04 11:47:50 +0200

    Passing data into components with @Input

--- src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-count',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   templateUrl: 'passenger-count.component.html',
 })
 export class PassengerCountComponent {
-  constructor() {}
+  @Input()
+  items: Passenger[];
+
+  checkedInCount(): number {
+    if (!this.items) return 0;
+    return this.items.filter((passenger: Passenger) => passenger.checkedIn).length;
+  }
 }
