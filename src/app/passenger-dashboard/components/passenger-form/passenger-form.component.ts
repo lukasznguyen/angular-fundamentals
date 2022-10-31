c326d06da8da44df63ac813ac3dd2bfa1e33a8d7
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-10-31 18:25:42 +0100

    Form stateless component

---
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from "@angular/core";
+import {Passenger} from "../../models/passenger.interface";
+
+@Component({
+  selector: 'passenger-form',
+  styleUrls: ['passenger-form.component.scss'],
+  templateUrl: 'passenger-form.component.html'
+})
+export class PassengerFormComponent {
+  @Input()
+  detail: Passenger;
+}
