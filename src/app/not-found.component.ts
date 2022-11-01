6c0a789f48ee2534bbfd0d774fedcc15e9f22e7a
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-11-01 15:10:18 +0100

    Wildcard routes for 404 handling

---
+++ src/app/not-found.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'not-found',
+  styleUrls: ['not-found.component.scss'],
+  templateUrl: 'not-found.component.html'
+})
+export class NotFoundComponent {
+}
