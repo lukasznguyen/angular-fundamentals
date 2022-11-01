83e9f287fae7be15349e185200d82213028fd017
Lukasz Nguyen <tuan.viet@ppuc.pl>
2022-11-01 15:49:48 +0100

    Child and dynamic routes

--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -16,7 +16,10 @@ import {PassengerDashboardService} from './passenger-dashboard.service';
 const routes: Routes = [
   {
     path: 'passengers',
-    component: PassengerDashboardComponent
+    children: [
+      { path: '', component: PassengerDashboardComponent },
+      { path: ':id', component: PassengerViewerComponent },
+    ]
   }
 ]
 
